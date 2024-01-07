87a4aadaddfd6dc5547018fead6de7d25287d11a
Felipe Alves <134858772+Felipe-Alvess@users.noreply.github.com>
2024-01-06 22:15:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function clickMenu(){
+    if(itens.style.display == 'block'){
+     itens.style.display = 'none'
+ }else{
+     itens.style.display = 'block'
+ }
+ }
+ 
+ function fecharSubmenu(){
+    if(itens.style.display == 'block'){
+     itens.style.display = 'none'
+ }else{
+     itens.style.display = 'block'
+ }
+ }
